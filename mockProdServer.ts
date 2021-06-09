2c79cbb9eddf85d5a1647bd89ed8af786aaf77c8
luoxi <1031596654@qq.com>
2021-06-09 18:57:29 +0800

    补充线上Mock数据

--- mockProdServer.ts
+++ mockProdServer.ts
@@ -1,8 +1,10 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
 import userModule from './mock/user'
+import tableModule from './mock/table'
 
 export function setupProdMockServer() {
   createProdMockServer([
-    ...userModule
+    ...userModule,
+    ...tableModule
   ]);
 }
