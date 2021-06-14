aac1185945bcc827472491244f1b17f8e926f9c1
cmdparkour <1031596654@qq.com>
2021-06-14 23:49:07 +0800

    微小修改，未完善，todo

--- src/views/main/pages/crudTable/layer.vue
+++ src/views/main/pages/crudTable/layer.vue
@@ -1,11 +1,11 @@
 <template>
   <Layer :layer="layer" @confirm="submit">
-    <el-form :model="form" :rules="rules" ref="ruleForm" label-width="120px" style="margin-right:30px;">
+    <el-form :model="form" :rules="rules" ref="form" label-width="120px" style="margin-right:30px;">
       <el-form-item label="名称：" prop="name">
-        <el-input v-model="form.name" size="small" placeholder="请输入名称"></el-input>
+        <el-input v-model="form.name" placeholder="请输入名称"></el-input>
       </el-form-item>
-      <el-form-item label="数字：">
-        <el-input v-model="form.sort" oninput="value=value.replace(/[^\d]/g,'')" placeholder="只能输入正整数" size="small"></el-input>
+      <el-form-item label="数字：" prop="sort">
+        <el-input v-model="form.sort" oninput="value=value.replace(/[^\d]/g,'')" placeholder="只能输入正整数"></el-input>
       </el-form-item>
 			<el-form-item label="选择器：" prop="select">
 			  <el-select v-model="form.select" placeholder="请选择" clearable>
@@ -17,7 +17,7 @@
 					</el-option>
 				</el-select>
 			</el-form-item>
-      <el-form-item label="单选框：">
+      <el-form-item label="单选框：" prop="radio">
         <el-radio-group v-model="form.radio">
           <el-radio :label="0">最新开播</el-radio>
           <el-radio :label="1">最早开播</el-radio>
@@ -31,6 +31,7 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import Layer from '@/components/layer/index.vue'
+import { add, update } from '@/api/table'
 export default defineComponent({
   components: {
     Layer
@@ -49,21 +50,50 @@ export default defineComponent({
   },
   setup() {
     const form = ref({})
-    const rules = {}
+    const rules = {
+      name: [
+        { required: true, message: '请输入姓名', trigger: 'blur' },
+      ],
+      sort: [
+        { required: true, message: '请输入数字', trigger: 'blur' },
+      ],
+      select: [
+        { required: true, message: '请选择', trigger: 'blur' },
+      ],
+      radio: [
+        { required: true, message: '请选择', trigger: 'blur' },
+      ],
+    }
     const options = [
       { value: 1, label: '运动'},
       { value: 2, label: '健身'},
       { value: 3, label: '跑酷'},
       { value: 4, label: '街舞'},
     ]
-    function submit(){
-      alert('操作成功')
-    }
+    console.log(form)
     return {
       form,
       rules,
-      options,
-      submit
+      options
+    }
+  },
+  methods: {
+    submit() {
+      this.$refs['form'].validate((valid: boolean) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          return false;
+        }
+      });
+    },
+    // 新增提交事件
+    addForm() {
+
+    },
+    // 编辑提交事件
+    updateForm() {
+
     }
   }
 })
