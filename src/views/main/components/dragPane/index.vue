0e5502621f2c0e350d7af1f1afa2dc76a3fd78c6
luoxi <1031596654@qq.com>
2021-06-18 18:33:31 +0800

    0.23版本，重大更新，多个组件实现

--- src/views/main/components/dragPane/index.vue
+++ src/views/main/components/dragPane/index.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="layout-container">
-    <div class="layout-container-table">
-      <el-alert
-        title="预计2021年7月1日前上线"
-        type="info"
-        effect="dark">
-      </el-alert>
-    </div>
+    <splitpanes class="default-theme" style="height: 100%">
+        <pane min-size="20">
+          <div class="item" style="background-color: #fe5308;">1</div>
+        </pane>
+        <pane>
+          <splitpanes class="default-theme" horizontal>
+            <pane>
+              <div class="item" style="background-color: #fefe34;">2</div>
+            </pane>
+            <pane>
+              <div class="item" style="background-color: #cfea2b;">3</div>
+            </pane>
+            <pane>
+              <div class="item" style="background-color: #66b032;">4</div>
+            </pane>
+          </splitpanes>
+        </pane>
+        <pane>
+          <div class="item" style="background-color: #0492ce;">5</div>
+        </pane>
+      </splitpanes>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 export default defineComponent({
+  components: {
+    Splitpanes,
+    Pane
+  },
   setup() {
 
   }
@@ -20,5 +40,14 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-  
+  .item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Helvetica, Arial, sans-serif;
+    color: #333;
+    font-size: 5em;
+    width: 100%;
+    height: 100%;
+  }
 </style>
