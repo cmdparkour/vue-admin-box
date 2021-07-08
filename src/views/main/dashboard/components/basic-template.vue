bc47697cb2e26478a94587e6c546e1b2e2571e72
luoxi <1031596654@qq.com>
2021-07-08 18:43:08 +0800

    细节修改

--- src/views/main/dashboard/components/basic-template.vue
+++ src/views/main/dashboard/components/basic-template.vue
@@ -10,14 +10,14 @@
           <h4>vue-admin-template-ts</h4>
           <p>ts版本的基础模板，支持ts，国际化，去除所有多余的页面及路由</p>
           <el-link type="primary" href="http://vue-admin-box-ts.51weblove.com" target="_blank">demo</el-link>
-          <el-link type="primary" href="http://www.github.com/cmdparkour/vue-admin-box-ts" target="_blank">github地址</el-link>
+          <el-link type="primary" href="http://www.github.com/cmdparkour/vue-admin-box-template-ts" target="_blank">github地址</el-link>
           <el-link type="primary" href="https://gitee.com/cmdparkour/vue-admin-box-template-ts" target="_blank">码云地址</el-link>
         </div>
         <div class="item">
           <h4>vue-admin-template-js</h4>
           <p>js版本的基础模板，去除ts，去除国际化，去除所有多余的页面及路由</p>
           <el-link type="primary" href="http://vue-admin-box-js.51weblove.com" target="_blank">demo</el-link>
-          <el-link type="primary" href="http://www.github.com/cmdparkour/vue-admin-box-js" target="_blank">github地址</el-link>
+          <el-link type="primary" href="http://www.github.com/cmdparkour/vue-admin-box-template-js" target="_blank">github地址</el-link>
           <el-link type="primary" href="https://gitee.com/cmdparkour/vue-admin-box-template-js" target="_blank">码云地址</el-link>
         </div>
       </div>
