800f7dbd1658b3b15fd71e9f94692ba256834414
luoxi <1031596654@qq.com>
2021-07-23 18:36:59 +0800

    动态路由专用

---
+++ src/components/menu/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+
+  }
+})
+</script>
