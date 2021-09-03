d24456c05a21cb6a84c9daa3a289f2d20bb4ce83
cmdparkour <1031596654@qq.com>
2021-09-03 12:21:21 +0800

    忘记放出来了

--- src/router/modules/menu.ts
+++ src/router/modules/menu.ts
@@ -9,47 +9,47 @@ const route = [
     alwayShow: true,
     meta: { title: 'message.menu.menu.name', icon: 'el-icon-s-grid' },
     children: [
-      // {
-      //   path: 'menu-1',
-      //   component: createNameComponent(() => import('@/components/menu/index.vue')),
-      //   redirect: '/menu/menu-1/menu-1-1',
-      //   meta: { title: 'message.menu.menu.menu_1' },
-      //   children: [
-      //     {
-      //       path: 'menu-1-1',
-      //       component: createNameComponent(() => import('@/components/menu/index.vue')),
-      //       redirect: '/menu/menu-1/menu-1-1/menu-1-1-1',
-      //       meta: { title: 'message.menu.menu.menu_1_1' },
-      //       children: [
-      //         {
-      //           path: 'menu-1-1-1',
-      //           component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-1/menu-1-1-1.vue')),
-      //           meta: { title: 'message.menu.menu.menu_1_1_1' },
-      //         },
-      //         {
-      //           path: 'menu-1-1-2',
-      //           component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-1/menu-1-1-2.vue')),
-      //           meta: { title: 'message.menu.menu.menu_1_1_2' },
-      //         },
-      //       ]
-      //     },
-      //     {
-      //       path: 'menu-1-2',
-      //       component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-2.vue')),
-      //       meta: { title: 'message.menu.menu.menu_1_2' },
-      //     },
-      //   ]
-      // },
+      {
+        path: 'menu-1',
+        component: createNameComponent(() => import('@/components/menu/index.vue')),
+        redirect: '/menu/menu-1/menu-1-1',
+        meta: { title: 'message.menu.menu.menu_1' },
+        children: [
+          {
+            path: 'menu-1-1',
+            component: createNameComponent(() => import('@/components/menu/index.vue')),
+            redirect: '/menu/menu-1/menu-1-1/menu-1-1-1',
+            meta: { title: 'message.menu.menu.menu_1_1' },
+            children: [
+              {
+                path: 'menu-1-1-1',
+                component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-1/menu-1-1-1.vue')),
+                meta: { title: 'message.menu.menu.menu_1_1_1' },
+              },
+              {
+                path: 'menu-1-1-2',
+                component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-1/menu-1-1-2.vue')),
+                meta: { title: 'message.menu.menu.menu_1_1_2' },
+              },
+            ]
+          },
+          {
+            path: 'menu-1-2',
+            component: createNameComponent(() => import('@/views/main/menu/menu-1/menu-1-2.vue')),
+            meta: { title: 'message.menu.menu.menu_1_2' },
+          },
+        ]
+      },
       {
         path: 'menu-2',
         component: createNameComponent(() => import('@/views/main/menu/menu-2.vue')),
         meta: { title: 'message.menu.menu.menu_2' }
       },
-      // {
-      //   path: 'menu-3',
-      //   component: createNameComponent(() => import('@/views/main/menu/menu-3.vue')),
-      //   meta: { title: 'message.menu.menu.menu_3' }
-      // },
+      {
+        path: 'menu-3',
+        component: createNameComponent(() => import('@/views/main/menu/menu-3.vue')),
+        meta: { title: 'message.menu.menu.menu_3' }
+      },
     ]
   }
 ]
