01f46c4a30e558124078747ea5d99e18c8a13efd
罗茜 <luoxi@yupaowang.com>
2022-09-25 19:50:45 +0800

    fix: 异常处理

--- src/layout/Menu/index.vue
+++ src/layout/Menu/index.vue
@@ -31,7 +31,7 @@ export default defineComponent({
     const expandOneMenu = computed(() => store.state.app.expandOneMenu)
     const allRoutes = useRouter().options.routes
     const route = useRoute()
-    const activeMenu = computed(() => {
+    const activeMenu: any = computed(() => {
       const { meta, path } = route;
       if (meta.activeMenu) {
         return meta.activeMenu;
