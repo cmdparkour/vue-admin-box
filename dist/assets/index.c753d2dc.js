541a6cc8d558f7365233f8ddec875d963478b63b
罗茜 <luoxi@yupaowang.com>
2023-02-21 23:36:32 +0800

    feat: update the dist website

--- dist/assets/index.c753d2dc.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,d as r,r as t,o as n,x as s}from"./index.2257ad0b.js";const a=r({setup(){}});function c(p,_,i,d,f,u){const e=t("router-view");return n(),s(e)}var x=o(a,[["render",c]]);export{x as default};
