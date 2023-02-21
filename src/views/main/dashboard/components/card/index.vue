6b8175b071a5f34c1fd14d5d06cdcd9b87333d48
罗茜 <luoxi@yupaowang.com>
2023-02-22 00:12:11 +0800

    feat: update the pay info

--- src/views/main/dashboard/components/card/index.vue
+++ src/views/main/dashboard/components/card/index.vue
@@ -13,8 +13,8 @@ export default defineComponent({
   },
   setup() {
     const list = [
-      { id: 1, name: '访问人数', data: '200', color: '#4e73df', icon: 'sfont system-yonghu' },
-      { id: 2, name: '最新信息', data: '20', color: '#1cc88a', icon: 'sfont system-xiaoxi' },
+      { id: 1, name: '访问人数', data: '100000', color: '#4e73df', icon: 'sfont system-yonghu' },
+      { id: 2, name: '最新信息', data: '30', color: '#1cc88a', icon: 'sfont system-xiaoxi' },
       { id: 3, name: '库存数量', data: '20000', color: '#36b9cc', icon: 'sfont system-shuliang_mianxing' },
       { id: 4, name: '当月营收', data: '20,000', color: '#f6c23e', icon: 'sfont system-jindutiaoshouyidaozhang' }
     ]
--- src/views/main/dashboard/index.vue
+++ src/views/main/dashboard/index.vue
@@ -3,19 +3,22 @@
     <basic-template />
     <Card />
     <Charts />
+    <Communication />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import basicTemplate from './components/basic-template.vue'
+import Communication from './components/communication.vue'
 import Card from './components/card/index.vue'
 import Charts from './components/charts/index.vue'
 export default defineComponent({
   components: {
     Card,
     Charts,
-    basicTemplate
+    basicTemplate,
+    Communication,
   }
 })
 </script>
