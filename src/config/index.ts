06927f0895de27d1cf1acae84b80a7367bf443c9
罗茜 <luoxi@yupaowang.com>
2023-03-19 15:16:09 +0800

    feat: update the dist

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,13 @@
+/*
+ * @Date: 2022-05-22 20:44:25
+ * @Description: 
+ */
 const showLogo: Boolean = true; // 是否显示Logo顶部模块
 const systemTitle = 'message.system.title' // 系统名称，用于显示在左上角模块，登录模块、以及浏览器标题上使用,使用配置项
 const systemSubTitle = 'message.system.subTitle' // 系统提示信息，用于登录模块使用
 
 /** 使用后端路由 */
-const isBackMenu = true
+const isBackMenu = false
 export {
   systemTitle,
   systemSubTitle,
