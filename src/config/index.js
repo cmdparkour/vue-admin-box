9c6fd2ee15c466feaf19e4cc26a952247888307a
罗茜 <luoxi@yupaowang.com>
2023-08-04 17:12:30 +0800

    feat: config dir handle change

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
  * @Date: 2022-05-22 20:44:25
  * @Description: 
  */
-const showLogo: Boolean = true; // 是否显示Logo顶部模块
+const showLogo = true; // 是否显示Logo顶部模块
 const systemTitle = 'message.system.title' // 系统名称，用于显示在左上角模块，登录模块、以及浏览器标题上使用,使用配置项
 const systemSubTitle = 'message.system.subTitle' // 系统提示信息，用于登录模块使用
 
