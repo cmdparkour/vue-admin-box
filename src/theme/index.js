3c056c194e87585ad27d6a1464a50b9c44cd4fd6
罗茜 <luoxi@yupaowang.com>
2023-08-04 17:31:47 +0800

    feat: theme change

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,44 +1,4 @@
-
-export interface Colors {
-  name: '' // 国际化版本请提供国际化字符串如：message.system.setting.style.default，非国际化版本直接写字符串，如：默认菜单风格
-  menu: {
-    textColor: string
-    background: string
-    childrenBackground: string
-    hoverBackground: string
-    submenuActiveColor: string
-  }
-  logo: {
-    color: string
-    background: string
-  }
-  header: {
-    background: string
-    textColor: string
-    itemHoverColor: string
-    breadcrumbTextColor: string
-    borderColor: string
-    tabBackground: string
-  }
-  container: {
-    background: string
-    mainBackground: string
-  }
-  page: {
-    background: string
-    color: string
-    tipColor: string
-    borderColor: string
-  }
-}
-
-export interface Style {
-  default: Colors
-  light: Colors
-  dark: Colors,
-  [propName: string]: Colors
-}
-export const style: Style = {
+export const style = {
   'default': {
     name: 'message.system.setting.style.default',
     menu: {
