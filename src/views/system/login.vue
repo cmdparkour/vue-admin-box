68f357f1a02ca5c56c814f7a1f13ea8f172294d9
罗茜 <luoxi@yupaowang.com>
2023-08-04 18:30:54 +0800

    feat: 修复异常

--- src/views/system/login.vue
+++ src/views/system/login.vue
@@ -45,9 +45,6 @@
             登录
           </el-button>
         </el-form>
-        <div class="fixed-top-right">
-          <select-lang/>
-        </div>
       </div>
     </div>
   </div>
@@ -61,13 +58,9 @@ import { useRouter, useRoute } from 'vue-router'
 import type { RouteLocationRaw  } from 'vue-router'
 import { getAuthRoutes } from '@/router/permission'
 import { ElMessage } from 'element-plus'
-import selectLang from '@/layout/Header/functionList/word.vue'
 import loginLeftPng from '@/assets/login/left.jpg';
 
 export default defineComponent({
-  components: {
-    selectLang
-  },
   setup() {
     const store = useStore()
     const router = useRouter()
