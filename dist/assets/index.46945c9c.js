ffeb1d4a94c0b712afa77f0a9b5ecfa13c889b9b
brab <frontend_brab@gmail.com>
2024-03-04 23:17:38 +0800

    feat: update dist

---
+++ dist/assets/index.46945c9c.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,o as t,c as n}from"./index.17d3c514.js";const r=o({setup(){}});function s(a,c,p,_,d,f){return t(),n("div")}var m=e(r,[["render",s]]);export{m as default};
---
+++ dist/assets/index.b41fec17.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index.17d3c514.js";const n={};function o(t,a){return c(),r("div")}var s=e(n,[["render",o]]);export{s as default};
